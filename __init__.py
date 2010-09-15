e23a5404196e646edc00216375286917bd40fd88
Jonas Haag <jonas@lophus.org>
2010-09-15 22:50:43 +0200

    Dropped PIL. Using tinymagick now.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,7 @@
 from __future__ import division
 import os
-try:
-    from PIL import Image
-except ImportError:
-    import Image
+import tinymagick
+
 
 PROPOSED_FORMATS = {
     'png' : set(['svg', 'gif', 'png', 'bmp', 'pnm', 'pdf'])
@@ -26,7 +24,7 @@ def autosize(image, max_width, max_height, digits=None, as_int=False):
     maximum values (given in ``max_width`` and ``max_height``)
     are respected and the aspect ratio is preserved.
 
-    :param image: The original ``PIL.Image.Image``
+    :param image: The original ``tinymagick.Image``
     :param int digits: Number of digits the resulting `width` and `height` shall
                        be rounded to (or ``-1`` if no rounding shall be done)
 
@@ -35,29 +33,27 @@ def autosize(image, max_width, max_height, digits=None, as_int=False):
     """
     # The following code could be expressed in about two lines, but I think
     # readability is much more important than performance/elegance here.
-
-    width, height = image.size
-    aspect_ratio = width / height
+    aspect_ratio = image.width / image.height
 
     # calculate the ratio of the image and would-be heights and widths
     # and go on with the highest of the both.
-    if max_height and height >= max_height:
-        heights_ratio = height / max_height
+    if max_height and image.height >= max_height:
+        heights_ratio = image.height / max_height
     else:
         heights_ratio = None
-    if max_width and width >= max_width:
-        widths_ratio = width / max_width
+    if max_width and image.width >= max_width:
+        widths_ratio = image.width / max_width
     else:
         widths_ratio = None
 
     if heights_ratio is widths_ratio is None:
         # do nothing. no maximums given that would matter in any way
-        return image.size
+        return (image.width, image.height)
 
     if heights_ratio > widths_ratio:
-        h, w = max_height, width / heights_ratio
+        h, w = max_height, image.width / heights_ratio
     else:
-        w, h = max_width, height / widths_ratio
+        w, h = max_width, image.height / widths_ratio
 
     if as_int:
         assert digits is None, "`digits` can't be set if `as_int` is True"
@@ -70,21 +66,13 @@ def autosize(image, max_width, max_height, digits=None, as_int=False):
     return w, h
 
 def thumbnail(original_file, result_file, max_width=None, max_height=None):
-    image = Image.open(original_file)
-    width_height = autosize(image, max_width, max_height, as_int=True)
-    if width_height == image.size:
+    image = tinymagick.Image(original_file)
+    width, height = autosize(image, max_width, max_height, as_int=True)
+    if (width, height) == (image.width, image.height):
         return original_file
 
     result_file += recommend_thumbnail_format(
         os.path.splitext(original_file)[1]
     )
-
-    thumb = image.resize(width_height)
-    try:
-        thumb.save(result_file, optimize=True)
-    except:
-        # only images under a certain size can be optimized.
-        # If they can't an exception will be raised (I could not figure out
-        # _which_ exception, hence the "except:".
-        thumb.save(result_file) # try to save without optimization
+    image.thumbnail(result_file, width, height)
     return result_file
